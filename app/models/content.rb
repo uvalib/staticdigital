e9c5fe937753fbeae1ad958a4501d4d4dbbcf7e9
xw5d <xw5d@d-172-25-206-210.dhcp.virginia.edu>
2018-07-16 12:09:52 -0400

    Find and list all files, find next id, form show read-only id, index page lists all contents

--- app/models/content.rb
+++ app/models/content.rb
@@ -7,7 +7,7 @@ class Content
 
   def save
     # set the destination path to write the file
-    dest_path = File.join(content_dir,  @public ? 'public' : 'uva', next_id.to_s, @file.original_filename.to_s)
+    dest_path = File.join(Content.content_dir,  @public=="1" ? 'public' : 'uva', next_id.to_s, @file.original_filename.to_s)
     # Save the file
     puts "save #{@id},#{@public},#{@file.tempfile.path.inspect}----"
     puts dest_path
@@ -16,12 +16,22 @@ class Content
     FileUtils.mv(@file.tempfile.path, dest_path)
   end
 
-  def all
+  # Find and list all files
+  # Create a class wide method that's not limited to an object of class. use self.methodname
+  def self.all
     # TODO: Find and list all files
-    []
+    result = []
+    public = Dir.glob(File.join(File.join(content_dir, "public"), File.join("**", "*.*")))
+    uva = Dir.glob(File.join(File.join(content_dir, "uva"), File.join("**", "*.*")))
+    all = public + uva
+    all.each do | pdf |
+      path_array = pdf.split('/')
+      result << { id: path_array[-2], "path": pdf, "uva-only": (path_array[-3] == 'uva') }
+    end
+    result
   end
 
-  def content_dir
+  def self.content_dir
     content_dir = ENV['CONTENT_DIRECTORY']
     if !content_dir.nil? && !content_dir.empty?
       return content_dir
@@ -30,8 +40,11 @@ class Content
     end
   end
 
+  # Find the next valid id
   def next_id
     # Todo: find the next id
-    100
+    id_list = Content.all.collect{|x| x[:id]}
+    next_id = id_list.max.to_i + 1
+    next_id
   end
 end
